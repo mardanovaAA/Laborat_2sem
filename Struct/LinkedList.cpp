743723bed92d543d0797a7fbb6fcf53e8cb0aed5
mardanovaA <mardanova.aa@phystech.edu>
2021-03-07 19:53:38 +0300

    Fixed Linked List

--- Struct/LinkedList.cpp
+++ Struct/LinkedList.cpp
@@ -1,17 +1,19 @@
+//Допиши изменение первого и последнего элемента в функциях добавить/удалить в конце/начале. На случай пустого листа!!!
+
 #include <iostream>
 
 struct ListNode{
     //This is the struct of one list node;
     //It contains the value of this node and the pointer to the next node;
     int value;
-    int* ptr_next;
+    ListNode* ptr_next;
 };
 
 struct List{
     //This is the struct of our list with some values (maybe not);
     //It contains the pointer to first and last nodes and the length of our list;
-    int* first;
-    int* last;
+    ListNode* first;
+    ListNode* last;
     int len;
 };
 
@@ -28,9 +30,15 @@ void add_to_end(List* LinkedList, int val){
     ListNode* node = new ListNode;
     node->ptr_next = nullptr;
     node->value = val;
-    LinkedList->last->ptr_next = node;
-    LinkedList->last = node;
-    LinkedList->len++;
+    if (LinkedList->len == 0){
+        LinkedList->last = node;
+        LinkedList->first = node;
+        LinkedList->len++;
+    } else {
+        LinkedList->last->ptr_next = node;
+        LinkedList->last = node;
+        LinkedList->len++;
+    }
 }
 
 void add_to_begin(List* LinkedList, int val){
@@ -39,16 +47,20 @@ void add_to_begin(List* LinkedList, int val){
     node->value = val;
     LinkedList->first = node;
     LinkedList->len++;
+    if (LinkedList->len == 1){
+        LinkedList->last = node;
+    }
 }
 
 void delete_list(List* LinkedList){
     //this function delete the list;
     ListNode* cur_node = LinkedList->first;
-    for (int i = 0; i < LinkedList->len; i++){
+    for (int i = 0; i < LinkedList->len - 1; i++){
         ListNode* node2del = cur_node;
         cur_node = cur_node->ptr_next;
         delete node2del;
     }
+    delete cur_node;
     delete LinkedList;
 }
 
@@ -59,30 +71,44 @@ void remove_value(List* LinkedList, int val){
     ListNode* cur_node = LinkedList->first; //it is a pointer to current node;
     if (cur_node->value == val){
         //What if our value is equal to the first node
-        cur_node = LinkedList->first;
-        LinkedList->first = LinkedList->first->next_ptr;
+        LinkedList->first = LinkedList->first->ptr_next;
         delete cur_node;
-        Linkedlist->len--;
-    } else{
-        while ((cur_node->next_ptr->value != val) || (cur_node->ptr_next->ptr_next != nullptr)){
+        LinkedList->len--;
+    } else {
+        while ((cur_node->ptr_next->value != val) || (cur_node->ptr_next->ptr_next != nullptr)){
             //try to find the node with the val;
             cur_node = cur_node->ptr_next;
         }
-        if (cur_node->next_ptr->value == val){
+        if (cur_node->ptr_next->value == val){
             //We find the node with the same value;
-            ListNode* node2del = cur_node->next_ptr; // We will delete this node;
-            cur_node->next_ptr = cur_node->next_ptr->next_ptr;
+            ListNode* node2del = cur_node->ptr_next; // We will delete this node;
+            cur_node->ptr_next = cur_node->ptr_next->ptr_next;
             delete node2del;
             LinkedList->len--;
-        } else if (cur_node->next_ptr->value == val){
+        } else if (cur_node->ptr_next->value == val){
             //if the latest node is value;
-            ListNode* node2del = cur_node->next_ptr;
-            cur_node->next_ptr = nullptr;
+            ListNode* node2del = cur_node->ptr_next;
+            cur_node->ptr_next = nullptr;
             delete node2del;
-            LinkedLisd->last = cur_node;
+            LinkedList->last = cur_node;
             LinkedList->len--;
         }
     }
+}
+
+List* copy_list(List* List_old){
+    List* List_new = new List;
+    List_new = create_empty();
+    if (List_old->len != 0){
+        ListNode* node = new ListNode;
+        node = List_old->first;
+        add_to_end(List_new, node->value);
+        for (int i = 0; i < List_old->len - 1; i++){
+            node = node->ptr_next;
+            add_to_end(List_new, node->value);
+        }
+    }
+    return List_new;
 }
 
 void print_list(List* LinkedList){
@@ -109,9 +135,12 @@ List* reverse_list(List* LinkedList){
 void merge_lists(List* List_first, List* List_second){
     //This function changes the first list (joins the second List to the end of the first List) and delete the second;
     ListNode* cur_node = List_second->first;
-    for (int i = 0; i < List_second->len; i++){
+    if (List_second->len != 0){
         add_to_end(List_first, cur_node->value);
-        cur_node = cur_node->ptr_next;
+        for (int i = 0; i < List_second->len - 1; i++){
+            cur_node = cur_node->ptr_next;
+            add_to_end(List_first, cur_node->value);
+        }
     }
     delete_list(List_second);
 }
@@ -128,7 +157,6 @@ void add_i(List* LinkedList, int val, int number){
         for (int i = 0; i < number - 1; i++){
             cur_node = cur_node->ptr_next;
         }
-        ListNode* new_node = new ListNode;
         new_node->ptr_next = cur_node->ptr_next;
         new_node->value = val;
         cur_node->ptr_next = new_node;
@@ -138,14 +166,14 @@ void add_i(List* LinkedList, int val, int number){
 
 void del_i(List* LinkedList, int number){
     //This function delete the node on the number place (0. len-1);
-    if ((Linked_list->len == 1) || (LinkedList->len == 0)){
+    if ((LinkedList->len == 1) || (LinkedList->len == 0)){
         LinkedList->len = 0;
         LinkedList->first = nullptr;
         LinkedList->last = nullptr;
     } else if (number == 0) {
         //delete the first number;
         ListNode* cur_node = LinkedList->first;
-        LinkedList->first = cur_node->next_ptr;
+        LinkedList->first = cur_node->ptr_next;
         LinkedList->len--;
         delete cur_node;
     } else if (number == LinkedList->len - 1){
